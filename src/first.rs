f0aba98c85b14a607c7ce296495830506493a373
Otávio Pace <otavio.pace@pagar.me>
2019-02-11 22:41:53 -0200

    first: add push method on List

--- src/first.rs
+++ src/first.rs
@@ -1,10 +1,7 @@
+use std::mem;
+
 pub struct List {
     head: Link,
-}
-
-struct Node {
-    elem: i32,
-    next: List,
 }
 
 enum Link {
@@ -12,8 +9,21 @@ enum Link {
     More(Box<Node>),
 }
 
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        };
+
+        self.head = Link::More(Box::new(new_node));
     }
 }
