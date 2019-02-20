4fe0409a07ce0b1c8b80ceacb51390e90e882bc3
Otávio Pace <otavio.pace@pagar.me>
2019-02-19 21:26:49 -0300

    fourth: add IntoIter implementation for Iterator and add into_iter

--- src/fourth.rs
+++ src/fourth.rs
@@ -142,6 +142,21 @@ impl<T> Drop for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.0.pop_front()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
