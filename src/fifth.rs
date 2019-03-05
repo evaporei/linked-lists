e12caecf64a173454bb88cdc4529aee29eb06613
Otávio Pace <otaviopp8@gmail.com>
2019-03-05 15:05:55 -0300

    fifth: add push method

--- src/fifth.rs
+++ src/fifth.rs
@@ -16,4 +16,24 @@ impl<'a, T> List<'a, T> {
     pub fn new() -> Self {
         List { head: None, tail: None }
     }
+
+    pub fn push(&'a mut self, elem: T) {
+        let new_tail = Box::new(Node {
+            elem,
+            next: None,
+        });
+
+        let new_tail = match self.tail.take() {
+            Some(mut old_tail) => {
+                old_tail.next = Some(new_tail);
+                old_tail.next.as_mut().map(|node| &mut **node)
+            }
+            None => {
+                self.head = Some(new_tail);
+                self.head.as_mut().map(|node| &mut **node)
+            }
+        };
+
+        self.tail = new_tail;
+    }
 }
