0e5d2fd52a1b5135fd2c093a05e941519d077c4b
Otávio Pace <otaviopp8@gmail.com>
2019-03-05 20:28:52 -0300

    silly1: add push to stack

--- src/silly1.rs
+++ src/silly1.rs
@@ -13,4 +13,18 @@ impl<T> Stack<T> {
     pub fn new() -> Self {
         Stack { head: None }
     }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: None,
+        });
+
+        self.push_node(new_node);
+    }
+
+    fn push_node(&mut self, mut node: Box<Node<T>>) {
+        node.next = self.head.take();
+        self.head = Some(node);
+    }
 }
