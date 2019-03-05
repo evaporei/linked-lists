84e9b4badb9086936055117ec0465f53cc595c65
Otávio Pace <otaviopp8@gmail.com>
2019-03-05 16:42:02 -0300

    fifth: add pop method

--- src/fifth.rs
+++ src/fifth.rs
@@ -36,4 +36,17 @@ impl<'a, T> List<'a, T> {
 
         self.tail = new_tail;
     }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+
+            if self.head.is_none() {
+                self.tail = None;
+            }
+
+            head.elem
+        })
+    }
 }
