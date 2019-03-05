ed11b5298a12db4458ceb43c63d44c198054dab3
Otávio Pace <otaviopp8@gmail.com>
2019-03-05 20:26:21 -0300

    silly1: add basic stack structure

---
+++ src/silly1.rs
@@ -0,0 +1,16 @@
+pub struct Stack<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { head: None }
+    }
+}
